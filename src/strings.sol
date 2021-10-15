47de64e49089625bdeea22241d59d219b1c6267f
Ryan Hall <hall.ryan.r@gmail.com>
2021-10-15 14:31:50 -0400

    update to src & tests to sol 0.8

--- src/strings.sol
+++ src/strings.sol
@@ -53,7 +53,10 @@ library strings {
         }
 
         // Copy remaining bytes
-        uint mask = 256 ** (32 - len) - 1;
+        uint mask = type(uint).max;
+        if (len > 0) {
+            mask = 256 ** (32 - len) - 1;
+        }
         assembly {
             let srcpart := and(mload(src), not(mask))
             let destpart := and(mload(dest), mask)
@@ -83,23 +86,23 @@ library strings {
         uint ret;
         if (self == 0)
             return 0;
-        if (uint(self) & 0xffffffffffffffffffffffffffffffff == 0) {
+        if (uint(self) & type(uint128).max == 0) {
             ret += 16;
             self = bytes32(uint(self) / 0x100000000000000000000000000000000);
         }
-        if (uint(self) & 0xffffffffffffffff == 0) {
+        if (uint(self) & type(uint64).max == 0) {
             ret += 8;
             self = bytes32(uint(self) / 0x10000000000000000);
         }
-        if (uint(self) & 0xffffffff == 0) {
+        if (uint(self) & type(uint32).max == 0) {
             ret += 4;
             self = bytes32(uint(self) / 0x100000000);
         }
-        if (uint(self) & 0xffff == 0) {
+        if (uint(self) & type(uint16).max == 0) {
             ret += 2;
             self = bytes32(uint(self) / 0x10000);
         }
-        if (uint(self) & 0xff == 0) {
+        if (uint(self) & type(uint8).max == 0) {
             ret += 1;
         }
         return 32 - ret;
@@ -211,12 +214,12 @@ library strings {
             }
             if (a != b) {
                 // Mask out irrelevant bytes and check again
-                uint256 mask = type(uint256).max; // 0xffff...
+                uint mask = type(uint).max; // 0xffff...
                 if(shortest < 32) {
                   mask = ~(2 ** (8 * (32 - shortest + idx)) - 1);
                 }
                 unchecked {
-                    uint256 diff = (a & mask) - (b & mask);
+                    uint diff = (a & mask) - (b & mask);
                     if (diff != 0)
                         return int(diff);
                 }
@@ -469,7 +472,10 @@ library strings {
 
         if (needlelen <= selflen) {
             if (needlelen <= 32) {
-                bytes32 mask = bytes32(~(2 ** (8 * (32 - needlelen)) - 1));
+                bytes32 mask;
+                if (needlelen > 0) {
+                    mask = bytes32(~(2 ** (8 * (32 - needlelen)) - 1));
+                }
 
                 bytes32 needledata;
                 assembly { needledata := and(mload(needleptr), mask) }
@@ -509,7 +515,10 @@ library strings {
 
         if (needlelen <= selflen) {
             if (needlelen <= 32) {
-                bytes32 mask = bytes32(~(2 ** (8 * (32 - needlelen)) - 1));
+                bytes32 mask;
+                if (needlelen > 0) {
+                    mask = bytes32(~(2 ** (8 * (32 - needlelen)) - 1));
+                }
 
                 bytes32 needledata;
                 assembly { needledata := and(mload(needleptr), mask) }
